d52192062ac056798440d0283515efd28e5228f5
tfleischauer <tfleischauer@gmail.com>
2015-07-23 00:14:47 -0700

    remove class
    remove class="blog-post"

--- page-about.php
+++ page-about.php
@@ -18,7 +18,7 @@
 	<?php while (have_posts()) : the_post(); ?> 
     
     <!-- START BLOG POST -->
-	<div class="blog-post">        
+	<div>        
         
         
         <!-- START BLOG-DESCRIPTION -->
